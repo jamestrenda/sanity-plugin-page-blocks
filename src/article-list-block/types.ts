4aa2c0586cd6a7fa11652182be38519e9bd8a8ad
James Trenda <jamestrenda@gmail.com>
2025-03-18 21:28:01 -0400

    chore(article-list-block): refactor type definitions

--- src/article-list-block/types.ts
+++ src/article-list-block/types.ts
@@ -1,37 +1,9 @@
-import {
-  DocumentComponents,
-  FieldDefinition,
-  FieldGroupDefinition,
-  FieldsetDefinition,
-  PreviewConfig,
-  TitledListValue,
-} from 'sanity'
+import {FieldDefinition, TitledListValue} from 'sanity'
+import {SchemaBaseFields} from '../types'
 
 type ArticleListBlockFieldNames = 'articleType' | 'title' | 'categories'
 
-// Allows assigning default fields to user-defined groups
-type GroupAssignment = {
-  field: ArticleListBlockFieldNames // Type-checked against existing fields
-  group: string | string[] // Can be any string from user-defined groups
-}
-
-type FieldsetAssignment = {
-  field: ArticleListBlockFieldNames
-  fieldset: string
-}
-
-interface SchemaBaseFields {
-  name?: string
-  fieldsets?: Array<FieldsetDefinition>
-  fieldsetAssignments?: Array<FieldsetAssignment>
-  groups?: Array<FieldGroupDefinition>
-  groupAssignments?: Array<GroupAssignment>
-  preview?: PreviewConfig
-  customFields?: Array<FieldDefinition>
-  customComponents?: DocumentComponents
-}
-
-interface Config extends SchemaBaseFields {
+interface Config extends SchemaBaseFields<ArticleListBlockFieldNames> {
   articleTypes?: (string | TitledListValue<string>)[] | undefined
   header?: FieldDefinition
   categoryField?: FieldDefinition
